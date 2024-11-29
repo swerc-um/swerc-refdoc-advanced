f5bf134aabb653dc82bb0e89bea406e49c1f22bb
Sarah Michel <sarah.michel01@etu.umontpellier.fr>
2024-11-29 11:36:21 +0100

    final

--- content/combinatorial/multinomial.h
+++ content/combinatorial/multinomial.h
@@ -2,7 +2,8 @@
  * Author: Mattias de Zalenski, Fredrik Niemelä, Per Austrin, Simon Lindholm
  * Date: 2002-09-26
  * Source: Max Bennedich
- * Description: Computes $\displaystyle \binom{k_1 + \dots + k_n}{k_1, k_2, \dots, k_n} = \frac{(\sum k_i)!}{k_1!k_2!...k_n!}$.
+ * Description: Count the number of ways to divide $k_1 + k_2 + \cdots + k_n$ objects into $n$ groups of sizes $k_1, k_2, \ldots, k_n$.\\
+ * Computes $\displaystyle \binom{k_1 + \dots + k_n}{k_1, k_2, \dots, k_n} = \frac{(\sum k_i)!}{k_1!k_2!...k_n!}$.
  * Status: Tested on kattis:lexicography
  */
 
